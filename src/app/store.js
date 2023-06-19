4c5fb2a3e7cf27fe0056a7217537ae47d624f6da
sajid1545 <clashking1545@gmail.com>
2023-06-19 22:14:14 +0600

    api initial setup done in store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
+import apiSlice from '../features/api/apiSlice';
 import authSlice from '../features/auth/authSlice';
-
 
 const store = configureStore({
 	reducer: {
 		auth: authSlice,
+		[apiSlice.reducerPath]: apiSlice.reducer,
 	},
+
+	middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
 });
 
 export default store;
