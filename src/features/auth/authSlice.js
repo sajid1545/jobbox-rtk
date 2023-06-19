07b5c7da2588170c4cfbe1bd941746c2c7b74e43
sajid1545 <clashking1545@gmail.com>
2023-06-19 18:19:46 +0600

    register using firebase implemented

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -1,4 +1,6 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import auth from './../../firebase/firebase.config';
 
 const initialState = {
 	user: { email: '', role: '' },
@@ -7,6 +9,11 @@ const initialState = {
 	error: '',
 };
 
+export const createUser = createAsyncThunk('auth/createUser', async ({ email, password }) => {
+	const data = await createUserWithEmailAndPassword(auth, email, password);
+	return data.user.email;
+});
+
 const authSlice = createSlice({
 	name: 'auth',
 	initialState,
@@ -14,6 +21,24 @@ const authSlice = createSlice({
 		logOut: (state) => {
 			state.user = { email: '', role: '' };
 		},
+	},
+	extraReducers: (builder) => {
+		builder
+			.addCase(createUser.pending, (state) => {
+				state.isLoading = true;
+				state.isError = false;
+			})
+			.addCase(createUser.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.isError = false;
+				state.user.email = payload;
+			})
+			.addCase(createUser.rejected, (state, action) => {
+				state.isLoading = false;
+				state.user.email = '';
+				state.isError = true;
+				state.error = action.error.message;
+			});
 	},
 });
 
--- src/layout/main/Navbar.js
+++ src/layout/main/Navbar.js
@@ -1,38 +1,63 @@
-import { signOut } from "firebase/auth";
-import React from "react";
+import React from 'react';
+import { useSelector } from 'react-redux';
 
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation } from 'react-router-dom';
 
 const Navbar = () => {
-  const { pathname } = useLocation();
+	const { pathname } = useLocation();
+	const {
+		user: { email, role },
+	} = useSelector((state) => state.auth);
 
-  return (
-    <nav
-      className={`h-14 fixed w-full z-[999] ${
-        pathname === "/" ? null : "bg-white"
-      }`}
-    >
-      <ul className='max-w-7xl mx-auto flex gap-3 h-full items-center'>
-        <li className='flex-auto font-semibold text-2xl'>
-          <Link to='/'>JobBox</Link>
-        </li>
-        <li>
-          <Link className='hover:text-primary' to='/jobs'>
-            Jobs
-          </Link>
-        </li>
+	return (
+		<nav className={`h-14 fixed w-full z-[999] ${pathname === '/' ? null : 'bg-white'}`}>
+			<ul className="max-w-7xl mx-auto flex gap-3 h-full items-center">
+				<li className="flex-auto font-semibold text-2xl">
+					<Link to="/">JobBox</Link>
+				</li>
+				<li>
+					<Link className="hover:text-primary" to="/jobs">
+						Jobs
+					</Link>
+				</li>
 
-        <li>
-          <Link
-            className='border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all '
-            to='/login'
-          >
-            Login
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
+				{email ? (
+					<li>
+						<Link className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all ">
+							Logout
+						</Link>
+					</li>
+				) : (
+					<li>
+						<Link
+							className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+							to="/login">
+							Login
+						</Link>
+					</li>
+				)}
+
+				{email && role && (
+					<li>
+						<Link
+							className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+							to="/dashboard">
+							Dashboard
+						</Link>
+					</li>
+				)}
+				{email && !role && (
+					<li>
+						<Link
+							className="border border-black px-2 py-1 rounded-full hover:border-primary hover:text-white hover:bg-primary hover:px-4 transition-all "
+							to="/register">
+							Get started
+						</Link>
+					</li>
+				)}
+			</ul>
+		</nav>
+	);
 };
 
 export default Navbar;
