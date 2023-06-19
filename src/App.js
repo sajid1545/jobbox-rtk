7d02ad681f8fa15ea711017dc74b4f1ff7ae8a91
sajid1545 <clashking1545@gmail.com>
2023-06-19 22:46:02 +0600

    get users handled

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { RouterProvider } from 'react-router-dom';
-import { setUser, toggleLoading } from './features/auth/authSlice';
+import { getUser, toggleLoading } from './features/auth/authSlice';
 import auth from './firebase/firebase.config';
 import routes from './routes/routes';
 
@@ -12,7 +12,7 @@ function App() {
 	useEffect(() => {
 		onAuthStateChanged(auth, (user) => {
 			if (user) {
-				dispatch(setUser(user.email));
+				dispatch(getUser(user.email));
 			} else {
 				dispatch(toggleLoading());
 			}
--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -19,6 +19,16 @@ export const createUser = createAsyncThunk('auth/createUser', async ({ email, pa
 	return data.user.email;
 });
 
+export const getUser = createAsyncThunk('auth/getUser', async (email) => {
+	const res = await fetch(`${process.env.REACT_APP_API_URL}/user/${email}`);
+	const data = await res.json();
+	if (data.status) {
+		return data;
+	}
+
+	return email;
+});
+
 export const loginUser = createAsyncThunk('auth/loginUser', async ({ email, password }) => {
 	const data = await signInWithEmailAndPassword(auth, email, password);
 	return data.user.email;
@@ -87,6 +97,25 @@ const authSlice = createSlice({
 				state.user.email = payload;
 			})
 			.addCase(googleLogin.rejected, (state, action) => {
+				state.isLoading = false;
+				state.user.email = '';
+				state.isError = true;
+				state.error = action.error.message;
+			})
+			.addCase(getUser.pending, (state) => {
+				state.isLoading = true;
+				state.isError = false;
+			})
+			.addCase(getUser.fulfilled, (state, { payload }) => {
+				state.isLoading = false;
+				state.isError = false;
+				if (payload.status) {
+					state.user = payload.data;
+				} else {
+					state.user.email = payload;
+				}
+			})
+			.addCase(getUser.rejected, (state, action) => {
 				state.isLoading = false;
 				state.user.email = '';
 				state.isError = true;
